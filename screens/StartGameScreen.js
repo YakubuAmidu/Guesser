633598fa7c0a3efb07f909604335f137a298ee06
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-20 02:32:43 -0700

    Commiting changes

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const StartGameScreen = (props) => {
-  return <View></View>;
+  return (
+    <View style={styles.screen}>
+      <Text>The Game Screen!</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
