5478c610786d5e240e0f9a24440e0e10ad4920f1
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-20 02:29:11 -0700

    Commiting changes

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const StartGameScreen = (props) => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+  },
+});
+
+export default StartGameScreen;
