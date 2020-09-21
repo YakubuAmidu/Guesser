bb5e969d493d85fad4017c4214996401bdab5ca7
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-21 00:42:00 -0700

    Commiting changes

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-const GameOverScreen = (props) => {};
+const GameOverScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Game is over</Text>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+});
 
 export default GameOverScreen;
