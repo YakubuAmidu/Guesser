95080df4777e9711f60006b8d7f045fd060e1cde
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-20 23:00:02 -0700

    Commiting changes

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 
-import Header from './components/Header';
-import StartGameScreen from './screens/StartGameScreen';
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
   return (
     <View style={styles.screen}>
-      <Header title='Guess a Number' />
+      <Header title="Guess a Number" />
       <StartGameScreen />
+      <GameScreen />
     </View>
   );
 }
